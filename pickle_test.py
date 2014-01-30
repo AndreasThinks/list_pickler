9e93ab9fba46fd2c7abbd86dc4675cd7debcb978
Crimsoneer <andrea.varotsis@gmail.com>
2014-01-30 16:45:09 +0000

    cleaned up comments and code

--- pickle_test.py
+++ pickle_test.py
@@ -6,14 +6,18 @@ class PicklerTests(unittest.TestCase):
     def setUp(self):
         os.open("pickle_test.p", os.O_RDWR|os.O_CREAT)
     def pickled_list_no_change(self):
+
+        #old code - ignore me
         #tester = open('pickle_test.p','r+')
-        my_list = list_pickler()[0]
-        file_loc = list_pickler()[1]
-        another_list = unpickler(file_loc)
         #my_list = ["cheese","ham"]
         #pickle.dump(my_list,tester)
         #another_list = pickle.load(tester)
         #tester.close()
+        
+        my_list = list_pickler()[0]
+        file_loc = list_pickler()[1]
+        another_list = unpickler(file_loc)
+
         self.assertEqual(my_list,another_list)
 
     def tearDown(self):
